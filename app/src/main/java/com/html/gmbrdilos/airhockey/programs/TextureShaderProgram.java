899ed3141db2070f2ad24e4050e773640722f635
ProjectGor <gor.hakobyan97@gmail.com>
2016-08-08 19:20:05 +0400

    tandzi poch

--- app/src/main/java/com/html/gmbrdilos/airhockey/programs/TextureShaderProgram.java
+++ app/src/main/java/com/html/gmbrdilos/airhockey/programs/TextureShaderProgram.java
@@ -1,7 +1,29 @@
 package com.html.gmbrdilos.airhockey.programs;
 
-/**
- * Created by iosuser7 on 8/8/16.
- */
+import android.content.Context;
+import android.opengl.GLES20;
+
+import com.html.gmbrdilos.airhockey.R;
+
 public class TextureShaderProgram {
+
+//     Uniform locations
+    private final int uMatrixLocation;
+    private final int uTextureUnitLocation;
+
+//     Attribute locations
+    private final int aPositionLocation;
+    private final int aTextureCoordinatesLocation;
+
+    public TextureShaderProgram(Context context) {
+        super(context, R.raw.texture_vertex_shader,
+                R.raw.texture_fragment_shader);
+//     Retrieve uniform locations for the shader program.
+        uMatrixLocation = GLES20.glGetUniformLocation(program, U_MATRIX);
+        uTextureUnitLocation = GLES20.glGetUniformLocation(program, U_TEXTURE_UNIT);
+
+//     Retrieve attribute locations for the shader program.
+        aPositionLocation = GLES20.glGetAttribLocation(program, A_POSITION);
+        aTextureCoordinatesLocation = GLES20.glGetAttribLocation(program, A_TEXTURE_COORDINATES);
+    }
 }
